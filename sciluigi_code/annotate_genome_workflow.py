bf74085a78141aa2f9aa1fe9ee7e578f2b7978d4
Sam Minot <sam@fredhutch.org>
2018-05-10 09:35:08 -0700

    CheckM Task, and workflow to run it with output from prokka

---
+++ sciluigi_code/annotate_genome_workflow.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python3
+"""Annotate a single genome."""
+
+import os
+import argparse
+import sciluigi as sl
+from general_tasks import LoadFile
+from assembly_tasks import AnnotateProkka, CheckM
+
+
+class AnnotateGenomeWorkflow(sl.WorkflowTask):
+    """ {genome sequence } -> [ prodigal / prokka ] -> {called peptides} -> [checkM] -> {completeness and taxID}"""
+
+    genome_fasta = sl.Parameter()
+    genome_name = sl.Parameter()
+    base_s3_folder = sl.Parameter()
+    aws_job_role_arn = sl.Parameter()
+    aws_s3_scratch_loc = sl.Parameter()
+    aws_batch_job_queue = sl.Parameter(default="optimal")
+    engine = sl.Parameter(default="aws_batch")
+    temp_folder = "/scratch"
+
+    def workflow(self):
+
+        # Load the input file
+        genome_fasta = self.new_task(
+            "load_genome_fasta",
+            LoadFile,
+            path=self.genome_fasta
+        )
+
+        # Run Prokka
+        annotate_prokka = self.new_task(
+            "annotate_prokka_{}".format(self.genome_name),
+            AnnotateProkka,
+            sample_name=self.genome_name,
+            output_folder=os.path.join(self.base_s3_folder, "prokka"),
+            threads=1,
+            temp_folder=self.temp_folder,
+            containerinfo=sl.ContainerInfo(
+                vcpu=1,
+                mem=4096,
+                engine=self.engine,
+                aws_s3_scratch_loc=self.aws_s3_scratch_loc,
+                aws_jobRoleArn=self.aws_job_role_arn,
+                aws_batch_job_queue=self.aws_batch_job_queue,
+                mounts={
+                    "/docker_scratch": {
+                        "bind": self.temp_folder,
+                        "mode": "rw"
+                    }
+                }
+            )
+        )
+
+        # Link the file for prokka annotation
+        annotate_prokka.in_fasta = genome_fasta.out_file
+
+        # Run CheckM
+        checkm = self.new_task(
+            "checkm_{}".format(self.genome_name),
+            CheckM,
+            sample_name=self.genome_name,
+            output_folder=os.path.join(self.base_s3_folder, "checkm"),
+            threads=8,
+            temp_folder=self.temp_folder,
+            containerinfo=sl.ContainerInfo(
+                vcpu=int(8),
+                mem=int(64),
+                engine=self.engine,
+                aws_s3_scratch_loc=self.aws_s3_scratch_loc,
+                aws_jobRoleArn=self.aws_job_role_arn,
+                aws_batch_job_queue=self.aws_batch_job_queue,
+                mounts={
+                    "/docker_scratch": {
+                        "bind": self.temp_folder,
+                        "mode": "rw"
+                    }
+                }
+            )
+        )
+
+        # Link the protein coding sequences from prokka into the inputs for checkm
+        checkm.in_faa = annotate_prokka.out_faa
+
+        return checkm
+
+
+if __name__ == "__main__":
+    """Annotate a bacterial genome."""
+
+    parser = argparse.ArgumentParser(description="""
+        Annotate a bacterial genome and run checkm.""")
+
+    parser.add_argument(
+        "--genome-fasta",
+        help="Location of FASTA file",
+        required=True
+    )
+
+    parser.add_argument(
+        "--genome-name",
+        help="Name of the genome (prefix for output files)",
+        required=True
+    )
+
+    parser.add_argument(
+        "--base-s3-folder",
+        help="Folder to store data in S3",
+        required=True
+    )
+
+    parser.add_argument(
+        "--aws-job-role-arn",
+        help="Job Role ARN to use with AWS Batch",
+        type=str
+    )
+
+    parser.add_argument(
+        "--aws-batch-job-queue",
+        help="Job Queue to use with AWS Batch",
+        type=str,
+        default="optimal"
+    )
+
+    parser.add_argument(
+        "--aws-s3-scratch-loc",
+        help="S3 bucket to use for scratch files",
+        type=str
+    )
+
+    parser.add_argument(
+        "--engine",
+        help="Execution engine",
+        type=str,
+        default="aws_batch"
+    )
+
+    parser.add_argument(
+        "--workers",
+        help="Number of workers to use for parallel execution",
+        type=int,
+        default=500
+    )
+
+    args = parser.parse_args()
+
+    sl.run(
+        main_task_cls=AnnotateGenomeWorkflow,
+        cmdline_args=[
+            "--{}={}".format(
+                k.replace("_", "-"),
+                v)
+            for k, v in args.__dict__.items()
+        ]
+    )
--- sciluigi_code/assembly_tasks.py
+++ sciluigi_code/assembly_tasks.py
@@ -111,3 +111,75 @@ class AnnotateProkka(sl.ContainerTask):
                 self.temp_folder
             ])
         )
+
+
+class CheckM(sl.ContainerTask):
+    # Input FASTP file of protein sequences
+    in_faa = None
+
+    # Sample name
+    sample_name = sl.Parameter()
+    # Output folder
+    output_folder = sl.Parameter()
+    # Number of threads to use
+    threads = sl.Parameter(default=4)
+    # Scratch directory
+    temp_folder = sl.Parameter(default="/scratch")
+
+    # URL of the container
+    container = "quay.io/fhcrc-microbiome/checkm:checkm-v1.0.11"
+
+    def out_tarball(self):
+        # Output is a tarball with all of the results
+        return sl.ContainerTargetInfo(
+            self,
+            os.path.join(
+                self.output_folder,
+                self.sample_name + ".tar.gz"
+            )
+        )
+
+    def run(self):
+
+        if self.output_folder.endswith("/") is False:
+            self.output_folder = self.output_folder + "/"
+
+        input_targets = {
+            "faa": self.in_faa()
+        }
+
+        output_targets = {
+            "tarball": self.out_tarball()
+        }
+
+        temp_dir = os.path.join(self.temp_folder, str(uuid.uuid4())[:8])
+
+        self.ex(
+            command="echo Checking to see if temp directory ({}) exists && ".format(temp_dir) +
+                    "[[ ! -d {} ]] && ".format(temp_dir) +
+                    "echo Making temp directory {} && ".format(temp_dir) +
+                    "mkdir {} && ".format(temp_dir) +
+                    "echo Making temp directory for input files {}/checkm_input && ".format(temp_dir) +
+                    "mkdir {}/checkm_input && ".format(temp_dir) +
+                    "echo Making temp directory for output files {}/checkm_output && ".format(temp_dir) +
+                    "mkdir {}/checkm_output && ".format(temp_dir) +
+                    "echo Moving gene FAA file into input directory && " +
+                    "mv $faa " + "{}/checkm_input/ && ".format(temp_dir) +
+                    "echo Decompressing input file && " +
+                    "gunzip {}/checkm_input/* && ".format(temp_dir) +
+                    "echo Running checkm && " +
+                    "checkm lineage_wf --genes -x faa {}/checkm_input/ {}/checkm_output/ && ".format(
+                        temp_dir, temp_dir
+                    ) + 
+                    "echo Finished running checkm && " +
+                    "ls -lhtr {}/checkm_output/ && ".format(temp_dir) +
+                    "echo Collecting results && " +
+                    "tar cvf {}/output.tar {}/checkm_output && ".format(temp_dir, temp_dir) +
+                    "echo Compressing results && " +
+                    "gzip {}/output.tar && ".format(temp_dir) +
+                    "echo Copying results out of the container && " +
+                    "mv {}/output.tar.gz ".format(temp_dir) + "$tarball",
+            input_targets=input_targets,
+            output_targets=output_targets
+            )
+
